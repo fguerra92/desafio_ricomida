05f7dcb33f4fa8611947889e4ae944c2f98630fe
Felipe <fguerra.ag@gmail.com>
2023-04-22 08:34:53 -0400

    carga index

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+
+  $(function(){
+    $("a").click(function(e){
+      if (this.hash !== ""){
+        e.preventDefault();
+        var url = this.hash;
+        $('html,body').animate({
+          scrollTop: $(url).offset().top
+        }, 800, function(){
+          window.location.hash = url;
+        });
+      }
+    });
+});
+
